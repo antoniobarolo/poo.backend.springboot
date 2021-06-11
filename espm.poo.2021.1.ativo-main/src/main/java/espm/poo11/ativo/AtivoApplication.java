117448db16f35c849e436259fd63459ec3766a65
antoniobarolo <antoniobarolog@gmail.com>
2021-06-11 18:47:49 -0300

    setup do ativo

---
+++ espm.poo.2021.1.ativo-main/src/main/java/espm/poo11/ativo/AtivoApplication.java
@@ -0,0 +1,13 @@
+package espm.poo11.ativo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtivoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtivoApplication.class, args);
+	}
+
+}
